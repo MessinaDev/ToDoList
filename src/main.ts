c73dc88b6fd3978723b7ca6189c76a5971484f9d
Daniele <messina@mida4.it>
2023-06-26 15:05:45 +0200

    delete plugin router

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from "./router";
+// import router from "./router";
 import vuetify from "./plugins/vuetify";
 
-createApp(App).use(createPinia()).use(router).use(vuetify).mount("#app");
+createApp(App).use(createPinia()).use(vuetify).mount("#app");
