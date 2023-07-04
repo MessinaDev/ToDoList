8d2c957cbae338f8845805b9dfeab5560589e526
Daniele <messina@mida4.it>
2023-07-04 14:28:43 +0200

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,45 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "To-Do",
+        short_name: "ToDo",
+        description: "Task list",
+        theme_color: "#181818",
+        icons: [
+          {
+            src: "./icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "./icons/maskable-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "./icons/512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "./icons/192.png",
+            sizes: "192x192",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
