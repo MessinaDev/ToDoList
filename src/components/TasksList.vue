01e4d44ed44f7886dcc5ff49483bfab87bab2ac8
Daniele <messina@mida4.it>
2023-07-07 16:54:13 +0200

    * refactor task list (WIP)

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -31,41 +31,19 @@
     </v-list>
   </section>
 
-  <section v-if="storeTasks.tasksCompletedByFilter.length && storeTasks.filterSelected != outdated"
-    :class="storeTasks.tasksCompletedByFilter.length ? 'mt-8' : ''">
-    <v-toolbar>
-      <v-toolbar-title>COMPLETED ({{ storeTasks.tasksCompletedByFilter.length }})</v-toolbar-title>
-      <v-btn variant="text" icon="mdi mdi-chevron-down" @click="showCompletedTasks = !showCompletedTasks"></v-btn>
-    </v-toolbar>
-    <v-list v-if="showCompletedTasks">
-      <v-list-item v-for="task in storeTasks.tasksCompletedByFilter" :key="task.id">
-        <v-list-item-title class="list-item__task-completed">
-          {{ task.description }}
-        </v-list-item-title>
-        <v-list-item-subtitle class="pl-2 ">
-          <v-icon class="mr-1">mdi mdi-clock-time-eight-outline</v-icon> {{ format(new Date(task.date), "dd/MM/yyyy") }}
-        </v-list-item-subtitle>
-
-        <template v-slot:append>
-          <v-btn variant="text" icon="mdi mdi-delete" size="large" class="list-item__task-completed--reset-decoration"
-            @click="deleteTask(task.id)">
-            <v-icon color="error"></v-icon>
-          </v-btn>
-        </template>
-      </v-list-item>
-    </v-list>
-  </section>
+  <TaskListCompleted />
 </template>
 
 <script setup lang="ts">
 import FilterTask from "./taskList/FilterTask.vue"
+import TaskListCompleted from "./taskList/TaskListCompleted.vue"
 import { useTasksStore } from "../stores/tasks"
 import { ref } from "vue"
 import { format, startOfDay } from "date-fns"
-import { outdated } from "../entities/taskFilter"
 
 const storeTasks = await useTasksStore();
 await storeTasks.fetchTasks();
+console.log("tasklist", storeTasks.tasksToCompleteByFilter)
 
 const showButtonReplaceTask = ref(true);
 // function initFieldsToReplaceTask(task: Task) {
@@ -78,15 +56,9 @@ function completeTask(id: string) {
 function deleteTask(id: string) {
   storeTasks.deleteTask(id);
 }
-
-let showCompletedTasks = ref(false);
 </script>
 
 <style scoped>
-.list-item__task-completed {
-  text-decoration: line-through;
-}
-
 .v-list-item {
   padding: 0px 16px;
 }
@@ -94,11 +66,5 @@ let showCompletedTasks = ref(false);
 .v-list-item--density-default.v-list-item--one-line {
   padding-top: 0px;
   padding-bottom: 0px;
-}
-
-@media (min-width: 1024px) {
-  header {
-    padding-right: 0;
-  }
 }
 </style>
--- src/components/taskList/TaskListCompleted.vue
+++ src/components/taskList/TaskListCompleted.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>
+  <section v-if="storeTasks.tasksCompletedByFilter.length && storeTasks.filterSelected != outdated"
+    :class="storeTasks.tasksCompletedByFilter.length ? 'mt-8' : ''">
+    <v-toolbar>
+      <v-toolbar-title>COMPLETED ({{ storeTasks.tasksCompletedByFilter.length }})</v-toolbar-title>
+      <v-btn variant="text" icon="mdi mdi-chevron-down" @click="showTasks = !showTasks"></v-btn>
+    </v-toolbar>
+    <v-list v-if="showTasks">
+      <v-list-item v-for="task in storeTasks.tasksCompletedByFilter" :key="task.id">
+        <v-list-item-title class="list-item__task-completed">
+          {{ task.description }}
+        </v-list-item-title>
+        <v-list-item-subtitle class="pl-2 ">
+          <v-icon class="mr-1">mdi mdi-clock-time-eight-outline</v-icon> {{ format(new Date(task.date), "dd/MM/yyyy") }}
+        </v-list-item-subtitle>
 
-  </div>
+        <template v-slot:append>
+          <v-btn variant="text" icon="mdi mdi-delete" size="large" class="list-item__task-completed--reset-decoration"
+            @click="deleteTask(task.id)">
+            <v-icon color="error"></v-icon>
+          </v-btn>
+        </template>
+      </v-list-item>
+    </v-list>
+  </section>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue"
+import { useTasksStore } from "../../stores/tasks"
+import { outdated } from "../../entities/taskFilter"
+import { format } from "date-fns"
 
+const storeTasks = await useTasksStore();
+console.log("tasklistcompleted", storeTasks.tasksToCompleteByFilter)
+
+let showTasks = ref(false);
+
+function deleteTask(id: string) {
+  storeTasks.deleteTask(id);
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-item__task-completed {
+  text-decoration: line-through;
+}
+
+header {
+  padding-right: 0;
+}
+</style>
